080dece7a04d15c320ca1a9774203c7aec8fdccd
AlokCdac <Alokdwivedifgiet@gmail.com>
2024-02-04 02:03:23 +0530

    data Validation added

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from mlProject import logger
 from mlProject.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.Stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Stage"
-logger.info(f">>>>>>>> stage{STAGE_NAME}startd <<<<<<<< ")
-data_ingestion=DataIngestionTrainingPipeline()
-data_ingestion.main()
+try:
 
-logger.info(f">>>>>>>>stage {STAGE_NAME} completed <<<<<<<")
+    logger.info(f">>>>>>>> stage{STAGE_NAME}startd <<<<<<<< ")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+
+    logger.info(f">>>>>>>>stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Data Validation"
+try:
+    logger.info(f">>>>>>>> stage{STAGE_NAME}startd <<<<<<<< ")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+
+    logger.info(f">>>>>>>>stage {STAGE_NAME} completed <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
 
